f01fe3e2ed9a941ca50b6301c0e805e26aa7eadf
Luke Winikates <luke.winikates@gmail.com>
2016-12-06 18:39:32 -0800

    add button on far right to add a column on that side

--- src/Splitscreen.elm
+++ src/Splitscreen.elm
@@ -12,9 +12,10 @@ import Splitscreen.Model exposing (Model, fromUrl, toUrl)
 
 -- TODO: test onload handler, feedback for failures to load
 -- TODO: make code generally cleaner
--- TODO: add button that adds a column to the layout
+-- TODO: add close button
 -- TODO: harmonize different data structures for "layout" (list of ints, list of list of (int, int), string representation)
 -- TODO: add a "play" button that turns columns into a carousel (possibly using css animations)
+
 
 main =
     Navigation.program UrlChange
@@ -72,54 +73,79 @@ iframeView : String -> String -> Html Msg
 iframeView key url =
     let
         positioning =
-            [ ( "border", "none" ), ( "width", "100%" ), ("height", "100%"), ("display", "block"), ("position", "absolute") ]
+            [ ( "border", "none" ), ( "width", "100%" ), ( "height", "100%" ), ( "display", "block" ), ( "position", "absolute" ) ]
     in
-        div [ style [ ( "position", "relative" ), ("flex-grow", "1") ] ]
+        div [ style [ ( "position", "relative" ), ( "flex-grow", "1" ) ] ]
             [ iframe [ src url, style positioning ] []
             , input
                 [ class "show-on-hover"
                 , placeholder "type a url here..."
                 , value url
                 , onInput (Change key)
-                , style [ ( "top", "0" ), ( "left", "0" ), ( "text-align", "center" ), ( "font-size", "24pt" ), ("position", "absolute"), ("width", "100%"), ("border", "none"), ("padding", "0")]
+                , style [ ( "top", "0" ), ( "left", "0" ), ( "text-align", "center" ), ( "font-size", "24pt" ), ( "position", "absolute" ), ( "width", "100%" ), ( "border", "none" ), ( "padding", "0" ) ]
                 ]
                 []
             ]
 
+
 appendToCol column modelLayout =
     List.indexedMap
-        (\index count -> count + if index == column then 1 else 0)
+        (\index count ->
+            count
+                + if index == column then
+                    1
+                  else
+                    0
+        )
         modelLayout
+
 
 layoutView : Model -> List (List ( Int, Int )) -> Html Msg
 layoutView model layout =
-    div [ style [ ( "display", "flex" ), ( "border-right", "1px solid white" ), ( "width", "calc(100vw - 10px)")] ]
-        (List.indexedMap
-            (\index  col ->
-                span [ style [ ( "flex-grow", "1" ), ("display", "flex"), ("flex-direction", "column"), ( "height", "calc(100vh - 10px)") ] ]
-                    (List.append (List.map
-                        (\( x, y ) ->
-                            let
-                                key =
-                                    "x" ++ toString x ++ "y" ++ toString y
-                            in
-                                iframeView key (withDefault "" (Dict.get key model.urls))
+    div [ style [ ( "display", "flex" ), ( "border-right", "1px solid white" ), ( "width", "calc(100vw - 10px)" ) ] ]
+        (List.append
+            (List.indexedMap
+                (\index col ->
+                    span [ style [ ( "flex-grow", "1" ), ( "display", "flex" ), ( "flex-direction", "column" ), ( "height", "calc(100vh - 10px)" ) ] ]
+                        (List.append
+                            (List.map
+                                (\( x, y ) ->
+                                    let
+                                        key =
+                                            "x" ++ toString x ++ "y" ++ toString y
+                                    in
+                                        iframeView key (withDefault "" (Dict.get key model.urls))
+                                )
+                                col
+                            )
+                            [ button
+                                [ onClick (Layout (appendToCol index model.layout))
+                                , style [ ( "bottom", "0" ), ( "width", "100%" ) ]
+                                ]
+                                [ text "+" ]
+                            ]
                         )
-                        col
-                    )
-                    [button [
-                    onClick (Layout (appendToCol index model.layout))
-                                 , style [("bottom", "0"), ("width", "100%")]]
-                                 [text "+"]])
+                )
+                layout
             )
-            layout
+            [ button
+                [ onClick (Layout (List.append model.layout [ 1 ]))
+                , style [ ( "right", "0" ), ( "height", "100%" ), ( "height", "calc(100vh - 10px)")]
+                ]
+                [ text "+" ]
+            ]
         )
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ node "style" [] [ text (".show-on-hover { transition: all 1s; background-color: transparent; color: transparent; }\n" ++
-                 ".show-on-hover:hover { background-color: #ccc; color: #111 }") ]
+        [ node "style"
+            []
+            [ text
+                (".show-on-hover { transition: all 1s; background-color: transparent; color: transparent; }\n"
+                    ++ ".show-on-hover:hover { background-color: #ccc; color: #111 }"
+                )
+            ]
         , layoutView model (modelToLayout model)
         ]
