fb3a9db2b632ed3668c404d7580e8866dc9a35ff
Luke Winikates <luke.winikates@gmail.com>
2016-12-10 16:22:58 -0800

    proof of concept for onload

--- src/Splitscreen.elm
+++ src/Splitscreen.elm
@@ -1,8 +1,8 @@
 module Splitscreen exposing (..)
 
 import Html exposing (Attribute, Html, a, body, button, div, form, h1, header, iframe, input, li, node, span, text, textarea, ul)
-import Html.Attributes exposing (href, placeholder, src, style, value)
-import Html.Events exposing (onInput, onClick, onSubmit, onMouseOver)
+import Html.Attributes exposing (attribute, href, placeholder, property, src, style, value)
+import Html.Events exposing (on, onClick, onInput, onMouseOver, onSubmit)
 import Navigation
 import List exposing (filter, head)
 import Maybe exposing (withDefault)
@@ -12,7 +12,8 @@ import Css.Helpers
 import Html.CssHelpers
 import Splitscreen.Model exposing (Model, appendToCol, fromUrl, key, modelToLayout, removeFromCol, toUrl, urlFor)
 import Splitscreen.Style exposing (..)
-
+import Json.Decode exposing (Value, decodeValue)
+import Json.Encode
 
 -- TODO: test onload handler, feedback for failures to load
 -- TODO: add a "play" button that turns columns into a carousel (possibly using css animations)
@@ -41,6 +42,7 @@ type Msg
     = Change String String
     | UrlChange Navigation.Location
     | LayoutChange (List Int)
+    | Debugging Value
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -56,6 +58,10 @@ update msg model =
             LayoutChange newLayout ->
                 wrap { model | layout = newLayout }
 
+            Debugging anything ->
+                let a = (Debug.log "load" anything) in
+                    (model, Cmd.none)
+
             UrlChange location ->
                 ( model, Cmd.none )
 
@@ -64,10 +70,21 @@ type alias GridPosition =
     ( Int, Int )
 
 
+onLoad : (Value -> msg) -> Attribute msg
+onLoad tagger =
+      on "load" (Json.Decode.map tagger Json.Decode.value)
+       -- it looks like we can find the url, at least, by looking at "event.path[0].src"
+       -- but what's the point? well, possibly we could have a timer waiting to hear about a successful load,
+       -- and if it didn't see one, it could show some help text "the most common reasons are:
+       -- 1> your url may not be correct. Is it a full url like http://example.com
+       -- 2> you may be trying to load a website that can't be loaded in an iframe.
+       -- . I'm not sure how to do the timing element there
+
+
 iframeView : ( Int, Int ) -> String -> Html Msg
 iframeView gridPosition url =
     div [ class [ Row ] ]
-        [ iframe [ src url, class [ UrlContent ] ] []
+        [ iframe [ src url , class [ UrlContent ],  onLoad Debugging ] []
         , input
             [ class [ ShowOnHover, UrlBar ]
             , placeholder "type a url here..."
